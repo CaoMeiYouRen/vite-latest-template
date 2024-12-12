9d02bb7b4bc66e9e10f91124512686e8fe9fd857
CaoMeiYouRen <996881204@qq.com>
2024-12-12 20:43:14 +0800

    chore: 迁移 yarn.lock 到 pnpm-lock.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    tsconfigPaths(),
-  ],
-  server: {
-    port: 4400,
-    open: true,
-    proxy: {},
-  },
+    plugins: [
+        vue(),
+        tsconfigPaths(),
+    ],
+    server: {
+        port: 4400,
+        open: true,
+        proxy: {},
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    build: {
+        chunkSizeWarningLimit: 1024,
+        minify: 'esbuild',
+    },
+    define: {
+        'process.env': {},
+    },
 })
