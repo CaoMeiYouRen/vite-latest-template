ddfa0768d1273b6761ea266c5e6cd4c2f5456ce9
CaoMeiYouRen <40430746+CaoMeiYouRen@users.noreply.github.com>
2025-11-11 11:04:54 +0800

    chore: 更新 eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,5 @@ import cmyr from 'eslint-config-cmyr/vue'
 export default defineConfig([cmyr, {
     files: ['**/*.{vue,ts,js}'],
     rules: {
-        '@typescript-eslint/no-empty-object-type': 1,
     },
 }])
